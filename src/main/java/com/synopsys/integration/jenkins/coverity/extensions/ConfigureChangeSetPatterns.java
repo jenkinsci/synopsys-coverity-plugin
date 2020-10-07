1c33711eb36e21f89d6ede7041a0a33c7dce4ac2
Richard Otte <rotte@blackducksoftware.com>
2020-10-07 10:44:28 -0400

    fix: correcting the help text of changeSetExclusionPatterns and changeSetInclusionPatterns

--- src/main/java/com/synopsys/integration/jenkins/coverity/extensions/ConfigureChangeSetPatterns.java
+++ src/main/java/com/synopsys/integration/jenkins/coverity/extensions/ConfigureChangeSetPatterns.java
@@ -34,19 +34,6 @@ import hudson.model.Descriptor;
 import net.sf.json.JSONObject;
 
 public class ConfigureChangeSetPatterns extends AbstractDescribableImpl<ConfigureChangeSetPatterns> {
-    @HelpMarkdown("Specify a comma separated list of filename patterns that you would like to explicitly included from the Jenkins change set.  \r\n"
-                      + "The pattern is applied to the $CHANGE_SET environment variable, and will affect which files are analyzed in an incremental analysis (cov-run-desktop).  \r\n"
-                      + "Examples:\r\n"
-                      + "\r\n"
-                      + "| File Name | Pattern    | Will be included |\r\n"
-                      + "| --------- | ---------- | ---------------- |\r\n"
-                      + "| test.java | *.java     | Yes              |\r\n"
-                      + "| test.java | *.jpg      | No               |\r\n"
-                      + "| test.java | test.*     | Yes              |\r\n"
-                      + "| test.java | test.????  | Yes              |\r\n"
-                      + "| test.java | test.????? | No               |")
-    private final String changeSetExclusionPatterns;
-
     @HelpMarkdown("Specify a comma separated list of filename patterns that you would like to explicitly excluded from the Jenkins change set.  \r\n"
                       + "The pattern is applied to the $CHANGE_SET environment variable, and will affect which files are analyzed in an incremental analysis (cov-run-desktop).  \r\n"
                       + "Examples:\r\n"
@@ -58,10 +45,23 @@ public class ConfigureChangeSetPatterns extends AbstractDescribableImpl<Configur
                       + "| test.java | test.*     | Yes              |\r\n"
                       + "| test.java | test.????  | Yes              |\r\n"
                       + "| test.java | test.????? | No               |")
+    private final String changeSetExclusionPatterns;
+
+    @HelpMarkdown("Specify a comma separated list of filename patterns that you would like to explicitly included from the Jenkins change set.  \r\n"
+                      + "The pattern is applied to the $CHANGE_SET environment variable, and will affect which files are analyzed in an incremental analysis (cov-run-desktop).  \r\n"
+                      + "Examples:\r\n"
+                      + "\r\n"
+                      + "| File Name | Pattern    | Will be included |\r\n"
+                      + "| --------- | ---------- | ---------------- |\r\n"
+                      + "| test.java | *.java     | Yes              |\r\n"
+                      + "| test.java | *.jpg      | No               |\r\n"
+                      + "| test.java | test.*     | Yes              |\r\n"
+                      + "| test.java | test.????  | Yes              |\r\n"
+                      + "| test.java | test.????? | No               |")
     private final String changeSetInclusionPatterns;
 
     @DataBoundConstructor
-    public ConfigureChangeSetPatterns(final String changeSetExclusionPatterns, final String changeSetInclusionPatterns) {
+    public ConfigureChangeSetPatterns(String changeSetExclusionPatterns, String changeSetInclusionPatterns) {
         this.changeSetExclusionPatterns = changeSetExclusionPatterns;
         this.changeSetInclusionPatterns = changeSetInclusionPatterns;
     }
@@ -91,7 +91,7 @@ public class ConfigureChangeSetPatterns extends AbstractDescribableImpl<Configur
         }
 
         @Override
-        public boolean configure(final StaplerRequest req, final JSONObject formData) throws Descriptor.FormException {
+        public boolean configure(StaplerRequest req, JSONObject formData) throws Descriptor.FormException {
             req.bindJSON(this, formData);
             save();
             return super.configure(req, formData);
